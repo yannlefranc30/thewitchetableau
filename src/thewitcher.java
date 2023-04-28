8f77961805e806a0fd952e6ee13bc61ab1479554
yann <yannlefranc330@gmail.com>
2023-04-28 13:14:28 +0200

    thewitchertableau

---
+++ src/thewitcher.java
@@ -0,0 +1,12 @@
+public class thewitcher {
+    public static void main( String [] args ){
+        // Initialisation du tableau
+        int [] myArray = { 10, 20, 30, 40, 50, 60, 70, 80, 90, 100 };
+
+        // On accède à certaines valeurs de ce tableau
+        System.out.println( myArray[0] );
+        System.out.println( myArray[5] );
+        System.out.println( myArray[9] );
+
+    }
+}
